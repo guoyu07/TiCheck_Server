e732c4789502f8b1171bff66db4a538000e66870
Ming.Yang <yougmark94@gmail.com>
2014-05-12 14:31:39 +0800

    更新携程机票数据库

--- TiCheck.sql
+++ TiCheck.sql
@@ -101,6 +101,78 @@ CREATE TABLE IF NOT EXISTS `HistoryPrice` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
+--
+-- Table structure for table `ctrip_Flight`
+--
+
+
+CREATE TABLE IF NOT EXISTS `ctrip_Flight` (
+	`DepartCityCode` char(3) COLLATE ascii_general_ci NOT NULL,
+	`ArriveCityCode` char(3) COLLATE ascii_general_ci NOT NULL,
+	`TakeOffTime` char(20) COLLATE ascii_general_ci NOT NULL,
+	`ArriveTime` char(20) COLLATE ascii_general_ci NOT NULL,
+	`Flight` char(6) COLLATE ascii_general_ci NOT NULL,
+	`CraftType` varchar(5) COLLATE ascii_general_ci NOT NULL,
+	`AirlineCode` char(2) COLLATE ascii_general_ci NOT NULL,
+	`Class` char(1) COLLATE ascii_general_ci NOT NULL,
+	`SubClass` char(1) COLLATE ascii_general_ci NOT NULL,
+	`DisplaySubclass` char(1) COLLATE ascii_general_ci NOT NULL,
+	`Rate` varchar(5) COLLATE ascii_general_ci NOT NULL,
+	`Price` varchar(8) COLLATE ascii_general_ci NOT NULL,
+	`StandardPrice` varchar(8) COLLATE ascii_general_ci NOT NULL,
+	`ChildStandardPrice` varchar(8) COLLATE ascii_general_ci NOT NULL,
+	`BabyStandardPrice` varchar(8) COLLATE ascii_general_ci NOT NULL,
+	`MealType` char(1) COLLATE ascii_general_ci NOT NULL,
+	`AdultTax` varchar(2) COLLATE ascii_general_ci NOT NULL,
+	`BabyTax` varchar(2) COLLATE ascii_general_ci NOT NULL,
+	`ChildTax` varchar(2) COLLATE ascii_general_ci NOT NULL,
+	`AdultOilFee` varchar(10) COLLATE ascii_general_ci NOT NULL,
+	`BabyOilFee` varchar(10) COLLATE ascii_general_ci NOT NULL,
+	`ChildOilFee` varchar(10) COLLATE ascii_general_ci NOT NULL,
+	`DPortCode` char(3) COLLATE ascii_general_ci NOT NULL,
+	`APortCode` char(3) COLLATE ascii_general_ci NOT NULL,
+	`DPortBuildingID` varchar(3) COLLATE ascii_general_ci NOT NULL,
+	`APortBuildingID` varchar(3) COLLATE ascii_general_ci NOT NULL,
+	`StopTimes` varchar(1) COLLATE ascii_general_ci NOT NULL,
+	`Nonrer` char(1) COLLATE ascii_general_ci NOT NULL,
+	`Nonend` char(1) COLLATE ascii_general_ci NOT NULL,
+	`Nonref` char(1) COLLATE ascii_general_ci NOT NULL,
+	`Rernote` varchar(255) COLLATE utf8_bin NOT NULL,
+	`Endnote` varchar(5) COLLATE utf8_bin NOT NULL,
+	`Refnote` varchar(127) COLLATE utf8_bin NOT NULL,
+	`Remarks` varchar(7) COLLATE utf8_bin NOT NULL,
+	`TicketType` varchar(4) COLLATE ascii_general_ci NOT NULL,
+	`BeforeFlyDate` varchar(1) COLLATE ascii_general_ci NOT NULL,
+	`Quantity` varchar(3) COLLATE ascii_general_ci NOT NULL,
+	`PriceType` varchar(15) COLLATE ascii_general_ci NOT NULL,
+	`ProductType` varchar(8) COLLATE ascii_general_ci NOT NULL,
+	`ProductSource` char(1) COLLATE ascii_general_ci NOT NULL,
+	`InventoryType` char(3) COLLATE ascii_general_ci NOT NULL,
+	`RouteIndex` varchar(1) COLLATE ascii_general_ci NOT NULL,
+	`NeedApplyString` varchar(1) COLLATE ascii_general_ci NOT NULL,
+	`Recommend` varchar(1) COLLATE ascii_general_ci NOT NULL,
+	`RefundFeeFormulaID` varchar(2) COLLATE ascii_general_ci NOT NULL,
+	`CanUpGrade` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`CanSeparateSale` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`CanNoDefer` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`IsFlyMan` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`OnlyOwnCity` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`IsLowestPrice` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`IsLowestCZSpecialPrice` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`PunctualityRate` varchar(3) COLLATE ascii_general_ci NOT NULL,
+	`PolicyID` varchar(6) COLLATE ascii_general_ci NOT NULL,
+	`AllowCPType` varchar(4) COLLATE ascii_general_ci NOT NULL,
+	`OutOfPostTime` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`OutOfSendGetTime` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`OutOfAirlineCounterTime` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`CanPost` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`CanAirlineCounter` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`CanSendGet` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`IsRebate` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`RebateAmount` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	`RebateCPCity` tinyint(1) COLLATE ascii_general_ci NOT NULL,
+	KEY (`DepartCityCode`, `ArriveCityCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_general_ci AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
